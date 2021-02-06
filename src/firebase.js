898c6af7f848b2f5de84a4a1833388dd3f94f226
Luca Gamerro <43759024+lucagamerro@users.noreply.github.com>
2021-02-07 00:50:42 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import firebase from 'firebase';
 import 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD20nV3v_M0jfuO8RCtedTDneUva-USbP0",
-    authDomain: "scambi-gasquemais.firebaseapp.com",
-    projectId: "scambi-gasquemais",
-    storageBucket: "scambi-gasquemais.appspot.com",
-    messagingSenderId: "893102789014",
-    appId: "1:893102789014:web:b0502d0b53837114070db7",
-    measurementId: "G-01ZKV4EB0R"
+    // Firebase config
 };
 
 firebase.initializeApp(firebaseConfig);
